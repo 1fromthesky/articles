dfe3af9dfccf13aff37e3a9d27a729f070a6ff04
Anton Kuzmenko <ayukuzmenko@gmail.com>
2018-12-24 21:54:54 +0300

    Add multi select

--- src/components/app.js
+++ src/components/app.js
@@ -42,6 +42,8 @@ export default class App extends Component {
                     value={selectedOption}
                     onChange={this.handleChange}
                     options = {this.selectOptions}
+                    closeMenuOnSelect={false}
+                    isMulti
                 />
                 <ArticleList
                     items = {articles}
