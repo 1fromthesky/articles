0327592466affb64c726b1c43974e0ae838d037c
Anton Kuzmenko <ayukuzmenko@gmail.com>
2019-01-12 02:43:32 +0300

    Refact

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+import { OrderedMap } from 'immutable';
+
+export const arrToMap = (arr, itemRecord) => {
+    return arr.reduce(
+        (acc, item) => acc.set(
+            item.id,
+            itemRecord ? new itemRecord(item) : item
+        ),
+        new OrderedMap());
+}
